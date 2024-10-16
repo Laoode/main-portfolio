02f139ddae7b2016af32f092535ef58811179223
LM Yudhy Prayitno <119650090+Yudhy-code-stranger@users.noreply.github.com>
2024-10-16 17:40:07 +0800

    Refactor: Remove Contentful integration and update portfolio page

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -7,12 +7,10 @@
 //     : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
 //   host: import.meta.env.DEV ? 'preview.contentful.com' : 'cdn.contentful.com',
 // })
-import { createClient } from 'contentful'
+// import { createClient } from 'contentful';
 
-export const contentfulClient = createClient({
-  space: import.meta.env.CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.DEV
-    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
-    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-  host: import.meta.env.DEV ? 'preview.contentful.com' : 'cdn.contentful.com',
-})
+// export const contentfulClient = createClient({
+//   space: 'k0pr0x62jffq', // Your Space ID
+//   accessToken: 'QZY1zK8VUd4afiIEYCEgVDiHWOM_pXwo8_oINxW7mUY', // Your Delivery Token
+//   host: 'cdn.contentful.com',
+// });
