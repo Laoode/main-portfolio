d503dc1cec0f6ae5e876d1dfe3914636d2094d27
LM Yudhy Prayitno <119650090+Yudhy-code-stranger@users.noreply.github.com>
2024-10-16 17:15:36 +0800

    Fix type errors and update timezone component

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,24 +1,24 @@
-import contentful from 'contentful'
+// import contentful from 'contentful'
 
-export const contentfulClient = contentful.createClient({
-  space: import.meta.env.CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.DEV
-    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
-    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-  host: import.meta.env.DEV ? 'preview.contentful.com' : 'cdn.contentful.com',
-})
+// export const contentfulClient = contentful.createClient({
+//   space: import.meta.env.CONTENTFUL_SPACE_ID,
+//   accessToken: import.meta.env.DEV
+//     ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+//     : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+//   host: import.meta.env.DEV ? 'preview.contentful.com' : 'cdn.contentful.com',
+// })
 
-// contentful.ts (continuation)
-export const fetchProjects = async () => {
-  const entries = await contentfulClient.getEntries({
-    content_type: 'projects', // Replace 'project' with your actual content type ID if it's different
-  });
+// // contentful.ts (continuation)
+// export const fetchProjects = async () => {
+//   const entries = await contentfulClient.getEntries({
+//     content_type: 'projects', // Replace 'project' with your actual content type ID if it's different
+//   });
 
-  return entries.items.map((item) => ({
-    heading: item.fields.title,
-    subheading: item.fields.description,
-    imagePath: item.fields.image.fields.file.url, 
-    href: item.fields.url,
-    altText: item.fields.title, // If you want to add an alt text for accessibility
-  }));
-};
+//   return entries.items.map((item) => ({
+//     heading: item.fields.title,
+//     subheading: item.fields.description,
+//     imagePath: item.fields.image.fields.file.url, 
+//     href: item.fields.url,
+//     altText: item.fields.title, // If you want to add an alt text for accessibility
+//   }));
+// };
